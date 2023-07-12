545d007e7e0ea06479020f6046e39e22c41881b4
root <root@Johnatan>
2023-07-12 16:11:26 -0500

    console v2

--- console.py
+++ console.py
@@ -2,12 +2,16 @@
 '''Definition of the class'''
 import json
 import cmd
+import shlex
+from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """class hbnb console command"""
 
     prompt = "(hbnb) "
+    list_class = ["BaseModel"]
 
     def do_quit(self, line):
         '''Quit command to exit the program\n'''
@@ -19,14 +23,35 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def emptyline(self):
-        '''
-        Description:
-        ************
-            Ignore the blank lines
-        '''
+        '''Ignore the blank lines'''
 
         pass
 
+    def do_create(self, line):
+        '''Creates a new instance an save it'''
+        if len(line) > 0:
+            if line in HBNBCommand.list_class:
+                instance_tmp = eval(line + "()")
+                instance_tmp.save()
+                print("{}".format(instance_tmp.id))
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_show(self, line):
+        '''Prints the string representation of an instance by id'''
+        
+        code_key = line.split()
+        if len(line) > 0:
+            dict_objets = models.storage.all()
+            key = code_key[0] + "." + code_key[1]
+            if key in dict_objets:
+                print(dict_objets[key])
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
