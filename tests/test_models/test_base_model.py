af18c157b91c6e4eb8aa50ec9d50a5082b227286
jegomezV <jegomezvalencia@gmail.com>
2023-07-12 14:33:08 -0500

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,41 +1,60 @@
 #!/usr/bin/python3
-'''Definition of the test for BaseModel'''
+"""Unittest module for the class BaseModel
+"""
+import os
 import unittest
-from models.base_model import BaseModel
 from datetime import datetime
+from models.base_model import BaseModel
+import models
 
 
-class test_BaseModel(unittest.TestCase):
-    '''
-    Description:
-    ************
-        Test for the class BaseModel
-    '''
+class TestBaseModel(unittest.TestCase):
+    """methods of the test for class BaseModel
+    """
+    def test_documentation(self):
+        """this checks all the documentation
+        of all the methods of the BaseModel class
+        """
+        self.assertIsNotNone(models.base_model.__doc__)
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
-    def test_BaseModel_(self):
-        '''
-        Description:
-        ------------
-            Test for id and his variables
-        '''
+    def test_from_dict(self):
+        """test from dict"""
+        b = BaseModel()
+        b.name = "Holberton"
+        b.my_number = 89
+        d1 = b.to_dict()
+        b2 = BaseModel(**d1)
+        self.assertIsNot(b, b2)
+        self.assertIsInstance(b2, BaseModel)
+        self.assertEqual(b2.id, b.id)
+        self.assertEqual(b2.created_at, b.created_at)
+        self.assertEqual(b2.updated_at, b.updated_at)
+        self.assertEqual(b2.name, b.name)
+        self.assertEqual(b2.my_number, b.my_number)
 
-        my_base = BaseModel()
-        my_base2 = BaseModel()
+    def test_uniqueId(self):
+        """this check if the instance
+        that are created has a unique id
+        """
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        self.assertNotEqual(instance1, instance2)
+        self.assertNotEqual(instance1.id, instance2.id)
 
-        #testing the attributes
-        self.assertNotEqual(my_base.id, my_base2.id)
-        self.assertTrue(my_base)
-        self.assertTrue(type(my_base.id) == str)
-        self.assertTrue(type(my_base.created_at) == datetime)
-        self.assertTrue(type(my_base.updated_at) == datetime)
-
-        #testing the methods
-        my_base_dict = my_base.to_dict()
-        my_base2_dict = my_base2.to_dict()
-        self.assertTrue(type(my_base_dict == dict))
-        self.assertNotEqual(my_base_dict, my_base2_dict)
-        my_base.save()
-        self.assertTrue(my_base.__str__)
+    def test_exec_permissions(self):
+        """Method that review if the file is executable
+        """
+        read = os.access('models/base_model.py', os.R_OK)
+        self.assertTrue(read)
+        write = os.access('models/base_model.py', os.W_OK)
+        self.assertTrue(write)
+        exect = os.access('models/base_model.py', os.X_OK)
+        self.assertTrue(exect)
 
     def test_typeData(self):
         """this method check the type of
@@ -46,3 +65,6 @@ class test_BaseModel(unittest.TestCase):
         self.assertIsInstance(instance1.updated_at, datetime)
         self.assertIsInstance(instance1.id, str)
         self.assertIsInstance(instance1, BaseModel)
+
+if __name__ == '__main__':
+    unittest.main()
