af81fb7c03565ad33c29bd797dab2658ef27521e
root <root@Johnatan>
2023-07-12 14:23:59 -0500

    console v1

--- console.py
+++ console.py
@@ -5,3 +5,29 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """class hbnb console command"""
     prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        '''
+        Description:
+        ***********
+            Command to exit the console
+        Args:
+        *****
+            Line (any): string with the command
+        '''
+        return True
+
+    def do_EOF(self, line):
+        '''
+        Description:
+        ************
+            Command to exit the console by EOF
+        Args:
+        *****
+            line (any): string with the command
+        '''
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
